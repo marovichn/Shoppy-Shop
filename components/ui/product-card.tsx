a461ae29b82229c0c03bfecf9760a7419bb66b99
marovichn <nikolamarovic.11@gmail.com>
2023-08-09 17:47:17 +0200

    Preview Modal, react-hot-toast

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -8,6 +8,7 @@ import { useRouter } from "next/navigation";
 import { Product } from "@/types";
 import IconButton from "./IconButton";
 import Currency from "./Currency";
+import usePreviewModal from "@/hooks/use-prview-modal";
 
 interface ProductCard {
   data: Product;
@@ -15,10 +16,17 @@ interface ProductCard {
 
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
   const router = useRouter();
+  const previewModal = usePreviewModal();
 
   const handleClick = () => {
     router.push(`/product/${data?.id}`);
   };
+
+  const onPreview: MouseEventHandler<HTMLButtonElement> =(event)=>{
+    event.stopPropagation();
+
+    previewModal.onOpen(data)
+  }
 
   return (
     <div
@@ -36,7 +44,7 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
         <div className='opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5'>
           <div className='flex gap-x-6 justify-center'>
             <IconButton
-              onClick={() => {}}
+              onClick={onPreview}
               icon={<Expand size={20} className='text-gray-600' />}
             />
             <IconButton
